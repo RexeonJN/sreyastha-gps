da458e5c2b723a02b397bdfd0f5b89bcb23a97e0
Kunal Dey <kunaldey.iitdhn@gmail.com>
2021-11-25 12:51:13 +0530

    cleared some bugs

--- lib/app/core/values/global_constants.dart
+++
@@ -1,1 +0,0 @@
-DateTime EXPIRY_DATE = DateTime(2021, 10, 1);
--- lib/app/core/values/keys.dart
+++
@@ -1,4 +0,0 @@
-Map<String, String> RAZORPAY_KEYS = {
-  "key_id": "rzp_test_U5YVfabNOHjkVg",
-  "key_secret": "JlN0NnXncRKLEp5SFAsztAVV",
-};
